e46c329347ec363c57f43e49e6a3fb79510def30
Bruno Binet <bruno.binet@gmail.com>
2014-04-09 09:18:55 +0200

    update graphite sink to remove use of stringio
    (to make it more similar to influxdb sink)

--- gstatsd/graphitesink.py
+++ gstatsd/graphitesink.py
@@ -1,5 +1,3 @@
-import cStringIO
-
 from gevent import socket
 
 from gstatsd.sink import Sink, E_SENDFAIL, compute_timer_stats
@@ -18,9 +16,9 @@ class GraphiteSink(Sink):
 
     @classmethod
     def encode(cls, stats, now, numstats=True):
-        buf = cStringIO.StringIO()
-        num_stats = 0
         now = int(now)  # time precision = second
+        num_stats = 0
+        lines = []
 
         # timer stats
         pct = stats.percent
@@ -35,27 +33,29 @@ class GraphiteSink(Sink):
                 'percent': pct,
                 }
             values.update(stats.timers_stats[key])
-            buf.write('%(key)s.mean %(mean)f %(now)d\n'
-                        '%(key)s.upper %(upper)f %(now)d\n'
-                        '%(key)s.upper_%(percent)d %(max_at_thresh)f %(now)d\n'
-                        '%(key)s.lower %(lower)f %(now)d\n'
-                        '%(key)s.count %(count)d %(now)d\n' % values)
+            lines.append(
+                '%(key)s.mean %(mean)f %(now)d\n'
+                '%(key)s.upper %(upper)f %(now)d\n'
+                '%(key)s.upper_%(percent)d %(max_at_thresh)f %(now)d\n'
+                '%(key)s.lower %(lower)f %(now)d\n'
+                '%(key)s.count %(count)d %(now)d\n' % values)
             num_stats += 1
 
         # counter stats
         for key, val in stats.counts.iteritems():
-            buf.write('stats.%(key)s %(count_interval)f %(now)d\n'
-                        'stats_counts.%(key)s %(count)f %(now)d\n' % {
-                            'key': key,
-                            'count': val,
-                            'count_interval': val / stats.interval,
-                            'now': now
-                            })
+            lines.append(
+                'stats.%(key)s %(count_interval)f %(now)d\n'
+                'stats_counts.%(key)s %(count)f %(now)d\n' % {
+                    'key': key,
+                    'count': val,
+                    'count_interval': val / stats.interval,
+                    'now': now
+                    })
             num_stats += 1
 
         # gauges stats
         for key, val in stats.gauges.iteritems():
-            buf.write('stats.%(key)s %(gauge)f %(now)d\n' % {
+            lines.append('stats.%(key)s %(gauge)f %(now)d\n' % {
                 'key': key,
                 'gauge': val,
                 'now': now
@@ -65,7 +65,7 @@ class GraphiteSink(Sink):
         # proxies stats
         for key, vals in stats.proxies.iteritems():
             for t, val in vals:
-                buf.write('stats.%(key)s %(val)s %(t)d\n' % {
+                lines.append('stats.%(key)s %(val)s %(t)d\n' % {
                     'key': key,
                     'val': val,
                     't': int(t)
@@ -73,15 +73,13 @@ class GraphiteSink(Sink):
             num_stats += len(vals)
 
         if numstats:
-            buf.write('statsd.numStats %(num_stats)d %(now)d\n' % {
+            lines.append('statsd.numStats %(num_stats)d %(now)d\n' % {
                 'num_stats': num_stats,
                 'now': now
                 })
 
-        out = buf.getvalue()
-        buf.close()
-        if out:
-            return out
+        if lines:
+            return ''.join(lines)
 
     def add(self, options):
         if isinstance(options, tuple):
@@ -97,8 +95,8 @@ class GraphiteSink(Sink):
     def send(self, stats, now):
         "Format stats and send to one or more Graphite hosts"
 
-        buf = self.__class__.encode(stats, now)
-        if not buf:
+        data = self.__class__.encode(stats, now)
+        if not data:
             return
 
         # TODO: add support for N retries
@@ -108,7 +106,7 @@ class GraphiteSink(Sink):
             try:
                 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
                 sock.connect(host)
-                sock.sendall(buf)
+                sock.sendall(data)
                 sock.close()
             except Exception, ex:
                 self.error(E_SENDFAIL % ('graphite', host, ex))
